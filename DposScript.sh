54124e5a4ce90be415f12c08bc270cd5c63d461e
gregorst3 <gregorydeveloper98@gmail.com>
2018-10-03 14:59:43 +0200

    Lisk and Rise

--- DposScript.sh
+++ DposScript.sh
@@ -4,7 +4,7 @@ MESSAGE="Server SERVERNAME Is not synced with the network"
 MEX="Server SERVERNAME Is DOWN"
 CHATID=
 apiToken=
-
+#Tested on Lisk and Rise
 TITLE="ATTENTION"
 ACCESS_TOKEN="PUSHBULLETTOKEN"
 
